97a3e8f0c2ad342efaf1f3e0a8f71e4c78836b21
josekatriel <josekatriel@gmail.com>
2024-06-22 01:16:08 +0700

    fix many thing

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     var navbarText = document.getElementById('navbar-text');
     var navbar = document.getElementById('navbar');
+    var lastScrollTop = 0;
 
     window.addEventListener('scroll', function () {
-        if (window.scrollY > 50) {
-            navbarText.classList.add('slide-out');
-            navbar.classList.add('outline', 'outline-2', 'outline-white');
+        var currentScroll = window.scrollY;
+        if (currentScroll > 50) {
+            if (currentScroll > lastScrollTop) {
+                // Scrolling down
+                navbarText.classList.add('slide-out');
+                navbar.classList.remove('slide-in');
+                navbar.classList.add('outline', 'outline-2', 'outline-white');
+            } else {
+                // Scrolling up
+                navbarText.classList.remove('slide-out');
+                navbarText.classList.add('slide-in');
+                navbar.classList.add('outline', 'outline-2', 'outline-white');
+            }
         } else {
             navbarText.classList.remove('slide-out');
+            navbarText.classList.add('slide-in');
             navbar.classList.remove('outline', 'outline-2', 'outline-white');
         }
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
     });
 
     // Smooth scrolling for navbar links
